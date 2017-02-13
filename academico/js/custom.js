d9e96aed88744226470c1fa2363d1368160e4d90
Spallacety <spallacety@hotmail.com>
2017-02-13 14:12:38 -0300

    Alteracoes login academico

--- academico/js/custom.js
+++ academico/js/custom.js
@@ -1,7 +1,30 @@
+  /* label color */
+  .input-field label {
+    color: #f44336;
+  }
+  /* label focus color */
+  .input-field input[type=text]:focus + label {
+    color: #f44336;
+  }
+  /* label underline focus color */
+  .input-field input[type=text]:focus {
+    border-bottom: 1px solid #f44336;
+    box-shadow: 0 1px 0 0 #f44336;
+  }
+  /* icon prefix focus color */
+  .input-field .prefix.active {
+    color: #f44336;
+  }
+      
 function hideShow(e) {
-        var display = document.getElementById(e).style.display;
-        if(display == "none")
-            document.getElementById(e).style.display = 'block';
-        else
-            document.getElementById(e).style.display = 'none';
-    }
+  var display = document.getElementById(e).style.display;
+    if(display == "none")
+      document.getElementById(e).style.display = 'block';
+    else
+      document.getElementById(e).style.display = 'none';
+}
+
+function hide(e){
+  document.getElementById(e).style.display = 'none';
+}
+
