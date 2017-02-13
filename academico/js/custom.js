e7af3d32594eb1760bdbc036e72cbbab8dc3e497
Spallacety <spallacety@hotmail.com>
2017-02-13 13:42:40 -0300

    Adicionado login academico

---
+++ academico/js/custom.js
@@ -0,0 +1,7 @@
+function hideShow(e) {
+        var display = document.getElementById(e).style.display;
+        if(display == "none")
+            document.getElementById(e).style.display = 'block';
+        else
+            document.getElementById(e).style.display = 'none';
+    }
