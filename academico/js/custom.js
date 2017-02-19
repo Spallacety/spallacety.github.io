6cbfdc5e1665a040e4ca1e74c5116ec1984fa07c
Spallacety <spallacety@hotmail.com>
2017-02-19 13:16:02 -0300

    Academico atualizado

--- academico/js/custom.js
+++ academico/js/custom.js
@@ -1,29 +1,53 @@
 $( window ).on( "load", function() {
-  document.getElementById(window.location.pathname).className = 'active';
-  $('.modal').modal();
+  var res = window.location.pathname.split("/");
+  document.getElementById(res.pop()).className = 'active';
   $('.tooltipped').tooltip({delay: 50});
-  $(".dropdown-button").dropdown();
+  $(".dropdown-button").dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false, // Does not change width of dropdown to that of the activator
+    hover: false, // Activate on hover
+    gutter: 0, // Spacing from edge
+    belowOrigin: true, // Displays dropdown below the button
+    alignment: 'left', // Displays dropdown with edge aligned to the left of button
+    stopPropagation: false // Stops event propagation
+  });
+
+   var tela = $(window).height();
+   var conteudo = $('navbar-professor').height + $('#content').height();
+
+   if ( conteudo < tela ) {
+      $('#footer').css({'position':'absolute','bottom':'0'});
+   }
 });
 
 $(document).ready(function() {
-  document.getElementById(window.location.pathname).className = 'active';
   $('.modal').modal();
-  $('.tooltipped').tooltip({delay: 50});
-  $(".dropdown-button").dropdown();
+  $('select').material_select();
 });
 
 $('#navbar-aluno').load("common/navbar-aluno.html");
+$('#navbar-professor').load("common/navbar-professor.html");
 $('#footer').load("common/footer.html");
 
 function verificarLogin() {
   if(document.getElementById("matricula").value == 'aluno'){
-    open("https://spallacety.github.io/academico/boletim", "_self");
+    open("boletim.html", "_self");
   }
   else if(document.getElementById("matricula").value == 'professor'){
-    open("https://spallacety.github.io/academico/professor", "_self");
+    open("chamada.html", "_self");
   }
   else{
     $('#error').modal('open');
     document.getElementById("senha").value = "";
   }
+}
+
+function abrirModalSair(){
+  $('#logout-modal').modal('open');
+}
+
+
+function abrirModalMatricula(){
+  $('#matricula-modal').modal('open');
 }
